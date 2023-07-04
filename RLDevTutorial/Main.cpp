07bb0c0624a66878bee0cc39439d27dcc37c2c03
itsallpulp <79062003+itsallpulp@users.noreply.github.com>
2023-07-04 12:47:33 -0400

    Added walking '@'

--- RLDevTutorial/Main.cpp
+++ RLDevTutorial/Main.cpp
@@ -6,9 +6,6 @@
 #include <SDL.h>
 
 #include "boost/lexical_cast.hpp" 
-
-
-
 #include "Globals.h"
 
 boost::uuids::random_generator uuidGenerator;
@@ -17,10 +14,12 @@ bool quit = false;
 
 int seed = 0;
 
+int pX, pY;
+
 int main(int argc, char* argv[])
 {
-	
-
+	pX = 0;
+	pY = 0;
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
@@ -30,7 +29,32 @@ int main(int argc, char* argv[])
 	Render::Init();
 	while (!quit)
 	{
-		
+		SDL_Event input;
+
+		while (SDL_PollEvent(&input))
+		{
+			if (input.type == SDL_KEYDOWN)
+			{
+				switch (input.key.keysym.sym)
+				{
+					case SDLK_UP:
+						--pY;
+						break;
+					case SDLK_DOWN:
+						++pY;
+						break;
+					case SDLK_RIGHT:
+						++pX;
+						break;
+					case SDLK_LEFT:
+						--pX;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		RenderAll();
 	}
 	return 0;
 }
@@ -42,5 +66,6 @@ std::string GenerateUUID()
 
 void RenderAll()
 {
-	
+	Render::Put(64, pX, pY, 255, 255, 255);
+	Render::Update();
 }
