b1c246a5f6359d0e9406f65e63f42fa71114518f
itsallpulp <79062003+itsallpulp@users.noreply.github.com>
2023-07-06 12:29:18 -0400

    Added random room placement

--- RLDevTutorial/Level.cpp
+++ RLDevTutorial/Level.cpp
@@ -1,45 +1,84 @@
 #include "Level.h"
 
-Level::Level()
+void Level::SetCell(Entity *t, int x, int y)
+{
+    delete mCells[x][y];
+    mCells[x][y] = t->Clone();
+    mCells[x][y]->cPhysics->x = x;
+    mCells[x][y]->cPhysics->y = y;
+}
+
+bool Level::AddRandomRoom()
+{
+    int x = (rand() % MAP_WIDTH / 2) * 2 + 1;
+    int y = (rand() % MAP_HEIGHT / 2) * 2 + 1;
+    int width = (rand() % 6) * 2 + 7,
+        height = (rand() % 4) * 2 + 7;
+
+    if (x + width >= MAP_WIDTH || y + height >= MAP_HEIGHT) { return false; }
+
+    for (int i = x; i < x+width; ++i)
+    {
+        for (int j = y; j < y + height; ++j)
+        {
+            if (!(mCells[i][j]->cPhysics->blocksMovement))
+            {
+                return false;
+            }
+        }
+    }
+
+    AddRoom({ x, y}, { x + width, y + height });
+
+    return true;
+}
+
+void Level::AddRoom(point topLeft, point bottomRight)
 {
     Entity *floor = new Entity("base_floor");
-    Entity *wall = new Entity("base_wall");
+    for (int x = topLeft.first +1 ; x < bottomRight.first -1; ++x)
+    {
+        for (int y = topLeft.second +1 ; y < bottomRight.second -1; ++y)
+        {
+            SetCell(floor, x, y);
+        }
+    }
 
+    delete floor;
+}
+
+void Level::StartMaze(int x, int y, Entity *tile)
+{
+}
+
+void Level::Fill(Entity *t)
+{
     for (int x = 0; x < MAP_WIDTH; ++x)
     {
         for (int y = 0; y < MAP_HEIGHT; ++y)
         {
-            mCells[x][y] = floor->Clone();
-            mCells[x][y]->cPhysics->x = x;
-            mCells[x][y]->cPhysics->y = y;
+            SetCell(t, x, y);
+        }
+    }
+}
+
+Level::Level()
+{
+    for (int x = 0; x < MAP_WIDTH; ++x)
+    {
+        for (int y = 0; y < MAP_HEIGHT; ++y)
+        {
+            mCells[x][y] = nullptr;
         }
     }
 
-    for (int x = 0; x < MAP_WIDTH; ++x)
-    {
-        delete mCells[x][0];
-        mCells[x][0] = wall->Clone();
-        mCells[x][0]->cPhysics->x = x;
-        mCells[x][0]->cPhysics->y = 0;
+    Entity *wall = new Entity("base_wall");
 
-        delete mCells[x][MAP_HEIGHT-1];
-        mCells[x][MAP_HEIGHT-1] = wall->Clone();
-        mCells[x][MAP_HEIGHT-1]->cPhysics->x = x;
-        mCells[x][MAP_HEIGHT-1]->cPhysics->y = MAP_HEIGHT-1;
-    }
+    Fill(wall);
 
-    for (int y = 0; y < MAP_HEIGHT; ++y)
-    {
-        delete mCells[0][y];
-        mCells[0][y] = wall->Clone();
-        mCells[0][y]->cPhysics->x = 0;
-        mCells[0][y]->cPhysics->y = y;
+    delete wall;
 
-        delete mCells[MAP_WIDTH-1][y];
-        mCells[MAP_WIDTH-1][y] = wall->Clone();
-        mCells[MAP_WIDTH-1][y]->cPhysics->x = MAP_WIDTH-1;
-        mCells[MAP_WIDTH-1][y]->cPhysics->y = y;
-    }
+    RoomsAndMazes();
 }
 
 Level::Level(json::object toLoad)
@@ -70,4 +109,14 @@ void Level::Render(int xOff, int yOff)
 Entity *Level::GetCell(int x, int y)
 {
     return mCells[x][y];
+}
+
+void Level::RoomsAndMazes()
+{
+    /* https://journal.stuffwithstuff.com/2014/12/21/rooms-and-mazes/ */
+    for (int i = 0; i < 1000; ++i)
+    {
+        AddRandomRoom();
+    }
+
 }
--- RLDevTutorial/Level.h
+++ RLDevTutorial/Level.h
@@ -10,6 +10,14 @@ class Level
 	private:
 	Entity *mCells[MAP_WIDTH][MAP_HEIGHT];
 
+	void SetCell(Entity *t, int x, int y);
+
+	bool AddRandomRoom();
+	void AddRoom(point topLeft, point bottomRight);
+	void StartMaze(int x, int y, Entity *tile);
+
+	void Fill(Entity *t);
+
 	public:
 	Level();
 	Level(json::object toLoad);
@@ -17,4 +25,6 @@ class Level
 	void LoadJson(json::object data);
 	void Render(int xOff = 0, int yOff = 0);
 	Entity* GetCell(int x, int y);
+
+	void RoomsAndMazes();
 };
